528f3436ac684a957117560fb221025e65991cbe
seif <seif.ehab0202@gmail.com>
2025-07-14 19:10:31 +0200

    intiate project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { ArrowUpRight } from "lucide-react";
 export default function Home() {
   return (
     <div className=" container">
-      <h1 className="heading1">seif1</h1>
-      <h2 className="heading2">seif2</h2>
-      <h3 className="heading3">seif3</h3>
-      <h4 className="heading4">seif4</h4>
-      <p className="heading5">seif5</p>
+      <h1 className="heading-1">seif1</h1>
+      <h2 className="heading-2">seif2</h2>
+      <h3 className="heading-3">seif3</h3>
+      <h4 className="heading-4">seif4</h4>
+      <p className="heading-5">seif5</p>
 
       <Button variant="primary">
         Contact us <ArrowUpRight className="size-5" />
