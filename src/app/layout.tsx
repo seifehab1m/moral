96a950941bdd11b065c3fc9799da36d6bf676324
Mohammed Abdelazem <moe.abdal93@gmail.com>
2025-07-21 08:01:53 +0300

    feat: initial header navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/layout/";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${myFont.variable} `}
       >
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Image from "next/image";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import Logo from "@/assets/logos/Logo";
+import { useWindowScroll } from "@/hooks/useWindowScroll";
+import { cn } from "@/lib/utils";
+
+type HeaderProps = {
+  forceDark?: boolean;
+};
+
+export function Header({ forceDark }: HeaderProps) {
+  const [scroll] = useWindowScroll();
+
+  const darkMode = (scroll?.y ?? 0) > 100 || !!forceDark;
+
+  console.log({ scroll, darkMode });
+
+  return (
+    <header
+      className={cn(
+        "lg:py-[52px] transition-all fixed top-0 left-1/2 -translate-x-1/2 z-50 flex items-center justify-between duration-500 w-full",
+        darkMode && "bg-white shadow lg:py-7",
+      )}
+    >
+      <div className="container w-full flex items-center justify-between">
+        <Link href="/">
+          <Logo
+            className={cn(
+              "transition-all text-white duration-500",
+              darkMode && "text-primary w-50",
+            )}
+          />
+        </Link>
+        <div className="flex items-center gap-8">
+          <div
+            className={cn(
+              "text-white font-semibold transition duration-500 flex items-center gap-1",
+              darkMode && "text-primary",
+            )}
+          >
+            <Link href="/">EN</Link>
+            <span>|</span>
+            <Link href="/">AR</Link>
+          </div>
+          <button className="cursor-pointer">
+            <Menu
+              size={30}
+              className={cn(
+                "text-white transition duration-500",
+                darkMode && "text-primary",
+              )}
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
