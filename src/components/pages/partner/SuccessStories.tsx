d067047af001f0125926c5b3c4fb103505d30071
seif <seif.ehab0202@gmail.com>
2025-07-24 21:14:14 +0200

    partner

--- src/components/pages/partner/SuccessStories.tsx
+++ src/components/pages/partner/SuccessStories.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import img from "@/assets/images/success.png";
+import img2 from "@/assets/images/success2.jpg";
 
 export function SuccessStories() {
   return (
     <section className="relative z-10 flex flex-col-reverse lg:flex-row lg:items-end justify-between text-white overflow-hidden ">
-      <Image className="object-cover ms-auto " src={img} alt="" />
+      <Image
+        className="hidden lg:block object-cover ms-auto"
+        src={img}
+        alt=""
+      />
+
+      {/* Mobile image (visible < lg) */}
+      <Image
+        className="block lg:hidden object-cover w-full "
+        src={img2}
+        alt=""
+      />
       <div
         className={cn(
-          "lg:absolute top-0 bottom-0 lg:translate-y-0 translate-y-12  z-10 px-4 py-6 bg-primary  rounded-ee-[52px] lg:rounded-ee-[200px] lg:w-1/2",
+          "lg:absolute top-0 bottom-0 lg:translate-y-0 translate-y-42 !pb-20  z-10 px-4 py-12 bg-primary  rounded-ee-[52px] lg:rounded-ee-[200px] lg:w-1/2",
           ""
         )}
       >
