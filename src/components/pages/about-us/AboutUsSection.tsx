0b5529693a791fad19bd3f6a90dc3093054ad6f7
seif <seif.ehab0202@gmail.com>
2025-08-16 15:54:24 +0200

    edits

--- src/components/pages/about-us/AboutUsSection.tsx
+++ src/components/pages/about-us/AboutUsSection.tsx
@@ -9,111 +9,97 @@ import { Navigation, Pagination } from "swiper/modules";
 // Timeline data based on your images
 const cards = [
   {
-    year: 1979,
-    title: "Founded",
-    description:
-      "Founder Mr. Rashid Bakht Alalasi started whole sale business.",
+    year: "1979",
+    title: "Founder Mr. Rashid Bakhit Alfalasi started whole sale business ",
+    // description:
+    //   "Founder Mr. Rashid Bakht Alalasi started whole sale business.",
   },
   {
-    year: 1989,
+    year: "1989",
     title:
-      "We entered the burgeoning real estate sector in Dubai and Sharjah, acquiring large parcels of land for development.",
+      "We entered the burgeoning real estate sector in Dubai and Sharjah, acquiring large parcels of land for development",
     description: "",
     // description:
     //   "We entered the burgeoning real estate sector in Dubai and Sharjah, acquiring large parcels of land for development.",
   },
   {
-    year: 1990,
-    title: "Started Contracting",
+    year: "1990",
+    title: "We started our first contracting company Buset Contracting",
     description: "We started our first contracting company, Buset Contracting.",
   },
   {
-    year: 1991,
-    title: "Entered construction & technical activities",
+    year: "1991",
+    title:
+      "We started construction of our real estate assets and Al Tala Technical Industries was established in Dubai to support our main contracting projects",
     description:
       "We started construction of our Al Taib Technical Industries unit in Dubai to support our main contracting projects.",
   },
   {
-    year: 1995,
-    title: "Completed major real estate projects",
+    year: "1995",
+    title:
+      "We completed major real estate projects, significantly boosting the entire group's growth and success. ",
     description:
       "We completed major real estate projects, significantly contributing to the entire group's growth and success.",
   },
   {
-    year: 2002,
-    title: "Family succession",
+    year: "2002",
+    title:
+      "Our founder, Mr. Rashid Bakhit Alfalasi, passed away, and leadership transitioned to Mr. Mohamed Rashed Alfalasi. Buset Investment was established to unify the family’s real estate assets.",
     description:
       "Our founder passed away and leadership transferred to Mr. Mohamed Rashid Alalasi. Buset Investment was established to unify the family’s real estate assets.",
   },
   {
-    year: 2003,
-    title: "Established Wooden Art Industries",
+    year: "2003",
+    title:
+      "To meet our in-house demand and create synergies, we established Wooden Art Industries in Dubai",
     description:
       "To meet our in-house demand and create synergies, we established Wooden Art Industries in Dubai.",
   },
   {
-    year: 2004,
-    title: "Expanded healthcare portfolio",
+    year: "2004",
+    title:
+      "We diversified our asset portfolio by launching our first state-of-the-art healthcare facility, Canadian Hospital Clinic, in response to Dubai's growing population",
     description:
       "We diversified our portfolio by launching our first state-of-the-art healthcare facility: Canadian Hospital Clinic, in response to Dubai’s growing population.",
   },
   {
-    year: 2007,
-    title: "Canadian Specialist Hospital built",
+    year: "2007",
+    title:
+      "Our healthcare expansion continued with the establishment of Dubai's first private 200-bed hospital, Canadian Specialist Hospital, in the heart of the city, equipped with the most advanced technology of the time.",
     description:
       "Our healthcare expansion continued with the establishment of Dubai’s first private 200-bed hospital, Canadian Specialist Hospital, with the most advanced technology of the time.",
   },
   {
-    year: 2009,
-    title: "Global recession response",
+    year: "2009",
+    title:
+      "Amidst the global recession, we successfully managed our assets by hedging our financial and operational positions through Buset Holding.",
     description:
       "Amidst the global recession, we integrated our portfolio by hedging our real estate and construction assets through Buset Holding.",
   },
   {
-    year: 2014,
-    title: "Entered hospitality sector",
+    year: "2014",
+    title:
+      "We reach another important goal: we stepped in the hospitality and opened our first fine dinning restaurant, Yashar Palace Restaurant",
     description:
       "We reached another growth milestone in the hospitality and opened our first dining venture: Yalnor Palace Restaurant.",
   },
   {
-    year: 2016,
-    title: "Expanded into hospitality via FAR Hotel Apartments",
+    year: "2016 - 2019",
+    title:
+      "Expanded into hospitality via RAF Hotel Apartments in 2016,We strengthen our presence in contracting by establishing Electromechanical Credible Contractors in 2018.Acquired USD 600 M worth of real estate assets in 2019.",
     description:
       "We expanded into hospitality via FAR Hotel Apartments in 2016.",
   },
+
   {
-    year: 2016,
-    title: "Entered engineering & electro-mechanical sector",
-    description:
-      "We strengthened our presence in contracting by establishing Electro-mechanical & Technical Construction in 2016.",
-  },
-  {
-    year: 2019,
-    title: "Acquired USD 600M real estate assets",
-    description:
-      "Acquired USD 600M worth of real estate assets in 2019. Moradi Holding was incorporated in 2019.",
-  },
-  {
-    year: 2021,
-    title: "Expanded Canadian Specialist Hospital",
+    year: "2021-2023",
+    title: "n 2021 We expanded the capacity of Canadian Specialist Hospital by 33%. In 2022, extend the investment to financial markets and maintained a portfolio of USD 70 M. Completed USD 600 M real estate project with whopping profits in 2023. Started restructuring of the group and added highly respected and qualified industry experts as leadership of the group in 2023",
     description:
       "We expanded the capacity of Canadian Specialist Hospital by 30% in 2021, seeking the industry’s highest standards and maintained a portfolio of more than 700M.",
   },
   {
-    year: 2022,
-    title: "USD 600M real estate project started",
-    description:
-      "Completed USD 600M real estate project with enhanced partners.",
-  },
-  {
-    year: 2023,
-    title: "High industry recognition",
-    description:
-      "Awarded highly protected and qualified industry expert as leadership of the group in 2023.",
-  },
-  {
-    year: 2024,
-    title: "Launched Moradi Holding as a global investment company",
+    year: "2024",
+    title: "We launched Moraal Holding as a global identity and established an ADGM-approved multi-strategy investment  fund to consolidate and manage our assets. This strategy has granted us access to global investments and preferential relationships with leading financial institutions worldwide",
     description:
       "We launched Moradi Holding as a global investment company with a multi-tiered investment fund, significantly expanding the group’s asset base and access to global capital. Created a global presence for healthcare assets and established relations with leading financial institutions worldwide.",
   },
@@ -160,7 +146,7 @@ export default function AboutUsSection() {
               <CardDates
                 title={c.title}
                 year={c.year}
-                description={c.description}
+                // description={c.description}
               />
             </SwiperSlide>
           ))}
--- src/components/pages/about-us/CardDates.tsx
+++ src/components/pages/about-us/CardDates.tsx
@@ -1,13 +1,13 @@
 type CardDatesProps = {
-  year: number;
+  year: string;
   title: string;
-  description: string;
+  description?: string;
 };
 
 export default function CardDates({
   year,
   title,
-  description,
+  // description,
 }: CardDatesProps) {
   return (
     <div className="relative aspect-[2/3] bg-white hover:bg-[#D8D8D6] duration-500 max-lg:min-w-[196px] max-w-[196px] lg:max-w-[300px] rounded group flex flex-col justify-between p-3.5 lg:p-4">
@@ -19,9 +19,9 @@ export default function CardDates({
           {title}
         </h6>
       </div>
-      <p className="text-secondary font-medium text-sm lg:text-base opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+      {/* <p className="text-secondary font-medium text-sm lg:text-base opacity-0 group-hover:opacity-100 transition-opacity duration-500">
         {description}
-      </p>
+      </p> */}
     </div>
   );
 }
